7d4aa2408c23851bec7efdb377e740491387c49e
nemanjat053 <tomi@tomi.com>
2020-04-29 16:10:51 +0200

    Added shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material.module';
+import { MaterialModule } from './shared/material/material.module';
 import { HomeComponent } from './home/home.component';
 import { UniverzitetComponent } from './univerzitet/univerzitet.component';
 import { FakultetComponent } from './fakultet/fakultet.component';
