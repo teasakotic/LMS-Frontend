a3d63e63932f6196d5709c69288ec4b8f9d6da51
nemanjat053 <tomi@tomi.com>
2020-05-03 23:13:10 +0200

    Fixed but turned off mapbox

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   mapbox: {
-    accessToken: 'pk.eyJ1IjoibG1uamFhIiwiYSI6ImNrOXI4NDJyNTBybnUzbnFwcDZhM3Jzd2gifQ.ih8HUYdzyRHUwUoF6t_0BA'
+    accessToken: 'API_KEY'
   }
 };
