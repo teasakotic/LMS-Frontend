b626a39fe65197a5b63003934c944240049d2a16
nemanjat053 <ntomanicn@gmail.com>
2020-07-01 08:28:04 +0200

    Pocetna stranica popravljena

--- src/app/map-mapbox/map-mapbox.component.ts
+++ src/app/map-mapbox/map-mapbox.component.ts
@@ -1,20 +1,15 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MapService } from './map.service';
 
 @Component({
   selector: 'app-map-mapbox',
   templateUrl: './map-mapbox.component.html',
-  styleUrls: ['./map-mapbox.component.css']
+  styleUrls: ['./map-mapbox.component.css'],
 })
 export class MapMapboxComponent implements OnInit {
-
-
-  constructor(private map: MapService) { }
+  constructor(private map: MapService) {}
 
   ngOnInit(): void {
-    // TODO: If you wanna view mapbox, clear comments
-    // this.map.buildMap();
+    this.map.buildMap();
   }
-
-
 }
--- src/app/map-mapbox/map.service.ts
+++ src/app/map-mapbox/map.service.ts
@@ -1,35 +1,37 @@
 import { Injectable } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
-import { environment } from "../../environments/environment";
+import { environment } from '../../environments/environment';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MapService {
   map: mapboxgl.Map;
   style = 'mapbox://styles/mapbox/streets-v11';
   lat = 45.267136;
   lng = 19.833549;
-  zoom = 13
+  zoom = 13;
   constructor() {
-    Object.getOwnPropertyDescriptor(mapboxgl, "accessToken").set('API_KEY');
+    Object.getOwnPropertyDescriptor(mapboxgl, 'accessToken').set(
+      'pk.eyJ1IjoibG1uamFhIiwiYSI6ImNrYzJ5dnRxNDI3eHEzM214NWV3ZW42OHcifQ.A-P57PuLyNo0o_0BdzgVww'
+    );
   }
   buildMap() {
     var map = new mapboxgl.Map({
-      container: "map",
-      style: "mapbox://styles/mapbox/streets-v11",
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
       zoom: 12,
-      center: [19.8409, 45.2525]
+      center: [19.8409, 45.2525],
     });
 
-    map.on("load", function () {
-      map.loadImage("https://i.imgur.com/MK4NUzI.png", function (error, image) {
+    map.on('load', function () {
+      map.loadImage('https://i.imgur.com/MK4NUzI.png', function (error, image) {
         if (error) throw error;
-        map.addImage("custom-marker", image);
+        map.addImage('custom-marker', image);
         map.addLayer({
-          id: "markers",
-          type: "symbol",
+          id: 'markers',
+          type: 'symbol',
           source: {
-            type: "geojson",
+            type: 'geojson',
             data: {
               type: 'FeatureCollection',
               features: [
@@ -37,21 +39,20 @@ export class MapService {
                   type: 'Feature',
                   properties: {},
                   geometry: {
-                    type: "Point",
-                    coordinates: [19.8445, 45.2531]
-                  }
-                }
-              ]
-            }
+                    type: 'Point',
+                    coordinates: [19.8445, 45.2531],
+                  },
+                },
+              ],
+            },
           },
           layout: {
-            "icon-image": "custom-marker",
-          }
+            'icon-image': 'custom-marker',
+          },
         });
       });
     });
 
     this.map.addControl(new mapboxgl.NavigationControl());
   }
-
 }
