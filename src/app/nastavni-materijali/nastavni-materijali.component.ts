d239e6c298fb8ef3ed9bf7c6a935bcc1cc69dd70
nemanjat053 <ntomanicn@gmail.com>
2020-07-01 09:33:30 +0200

    Added nastavni materijal; But need fixes

--- src/app/nastavni-materijali/nastavni-materijali.component.ts
+++ src/app/nastavni-materijali/nastavni-materijali.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { NastavniMaterijal } from '../models/nastavni-materijal';
+import { NastavniMaterijalService } from '../services/nastavni-materijal.service';
 
 @Component({
   selector: 'app-nastavni-materijali',
   templateUrl: './nastavni-materijali.component.html',
-  styleUrls: ['./nastavni-materijali.component.css']
+  styleUrls: ['./nastavni-materijali.component.css'],
 })
 export class NastavniMaterijaliComponent implements OnInit {
+  nastavniMaterijal: NastavniMaterijal;
 
-  constructor() { }
+  constructor(private nms: NastavniMaterijalService) {}
 
   ngOnInit(): void {
+    this.nms
+      .getNastavniMaterijal()
+      .subscribe((r) => (this.nastavniMaterijal = r));
   }
-
 }
