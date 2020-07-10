d5004eb99c9c66a061aadc19fb752d5322bfd2f9
nemanjat053 <ntomanicn@gmail.com>
2020-07-10 22:39:22 +0200

    Finished fakultet

--- src/app/fakultet/fakultet.component.ts
+++ src/app/fakultet/fakultet.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Fakultet } from '../models/fakultet';
 import { FakultetService } from '../services/fakultet.service';
+import { Email } from '../models/email';
+import { Telefon } from '../models/telefon';
 
 @Component({
   selector: 'app-fakultet',
@@ -9,13 +11,14 @@ import { FakultetService } from '../services/fakultet.service';
 })
 export class FakultetComponent implements OnInit {
   fakulteti: Fakultet[] = [];
+  emails: Email[] = [];
+  telefon: Telefon[] = [];
 
   constructor(private fs: FakultetService) {}
 
   ngOnInit(): void {
     this.fs.getFakulteti().subscribe((r) => {
       this.fakulteti = r;
-      console.log(this.fakulteti);
     });
   }
 }
--- src/app/services/fakultet.service.ts
+++ src/app/services/fakultet.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Fakultet } from '../models/fakultet';
+import { Email } from '../models/email';
 
 @Injectable({
   providedIn: 'root',
@@ -17,5 +18,13 @@ export class FakultetService {
 
   getFakultet(id: number): Observable<Fakultet> {
     return this.http.get<Fakultet>(this._url + `/${id}`);
+  }
+
+  getFakultetEmail(id: number): Observable<Email[]> {
+    return this.http.get<Email[]>(this._url + '/emails' + `/${id}`);
+  }
+
+  getFakultetTelefon(id: number): Observable<Email[]> {
+    return this.http.get<Email[]>(this._url + '/telefoni' + `/${id}`);
   }
 }
