7f6788468626cfa9b568702520d89c86d3427ad4
nemanjat053 <nemanjat053@example.com>
2020-07-11 19:12:34 +0200

    Fixed

--- src/app/models/nastavnik.ts
+++ src/app/models/nastavnik.ts
@@ -1,21 +1,12 @@
-import { Zvanje } from './zvanje';
 import { LicniPodaci } from './licni-podaci';
 import { RegistrovaniKorisnik } from './registrovani-korisnik';
-import { NastavnikNaRealizaciji } from './nastavnik-na-realizaciji';
 import { Adresa } from './adresa';
-import { Fakultet } from './fakultet';
-import { StudijskiProgram } from './studijski-program';
-import { Univerzitet } from './univerzitet';
 
 export interface Nastavnik {
-    id:number
-    biografija: String
-    zvanje: Zvanje
-    licniPodaci: LicniPodaci
-    registrovaniKorisnik: RegistrovaniKorisnik
-    nastavnikNaRealizaciji: NastavnikNaRealizaciji
-    adresa: Adresa
-    fakultet: Fakultet
-    studijskiProgram: StudijskiProgram
-    univerzitet: Univerzitet
+  id: number;
+  biografija: String;
+  licniPodaci: LicniPodaci;
+  registrovaniKorisnik: RegistrovaniKorisnik;
+  adresa: Adresa;
+  obrisan: boolean;
 }
