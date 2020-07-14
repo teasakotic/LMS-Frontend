53bda4c67aabcdf208d027fae239fcde50e13228
Zeljanaa <63678998+Zeljanaa@users.noreply.github.com>
2020-07-14 22:19:13 +0200

    Update disertacija.ts

--- src/app/models/disertacija.ts
+++ src/app/models/disertacija.ts
@@ -2,12 +2,12 @@ import { Nastavnik } from './nastavnik';
 import { DatotekaZaDisertaciju } from './datoteka-za-disertaciju';
 import { StudentNaGodini } from './student-na-godini';
 
-export interface Disertacija {
-    id: number
+export interface Disertacija { 
+    id: number 
     naslov: String
     datumOdbrane: Date
     datumAplikacije: Date
-    mentor: Nastavnik
+    mentor: Nastavnik 
     datotekaZaDizertaciju: DatotekaZaDisertaciju
     studentNaGodini: StudentNaGodini
 }
