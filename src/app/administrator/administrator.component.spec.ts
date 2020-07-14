5f0c15d0cb4ca675b9d2a7c4ba0df115e5f6dcb6
Teodora Šakotić <58300977+teasakotic@users.noreply.github.com>
2020-07-14 22:03:58 +0200

    AdministratorComponent

--- src/app/administrator/administrator.component.spec.ts
+++ src/app/administrator/administrator.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AdministratorComponent } from './administrator.component';
+import { AdministratorComponent } from './administrator.component'; 
 
 describe('AdministratorComponent', () => {
   let component: AdministratorComponent;
