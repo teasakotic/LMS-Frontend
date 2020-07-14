02f4cb3360a45d04e6f319f1c419e20fd416d7fa
Teodora Šakotić <58300977+teasakotic@users.noreply.github.com>
2020-07-14 22:07:11 +0200

    StudijskiProgramComponent

--- src/app/studijski-program/studijski-program.component.spec.ts
+++ src/app/studijski-program/studijski-program.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+
 import { StudijskiProgramComponent } from './studijski-program.component';
 
 describe('StudijskiProgramComponent', () => {
